651b8146b432cf6e6acc1ad582592669d923c3fa
caiofernandomf <caiofernandomf@gmail.com>
2021-12-13 01:18:02 -0300

    commit inicial

---
+++ src/main/java/net/springboot/caio/springbootmongosentencemanager/SpringbootMongoSentenceManagerApplication.java
@@ -0,0 +1,13 @@
+package net.springboot.caio.springbootmongosentencemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongoSentenceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongoSentenceManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/springboot/caio/springbootmongosentencemanager/SpringbootMongoSentenceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.springboot.caio.springbootmongosentencemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongoSentenceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
