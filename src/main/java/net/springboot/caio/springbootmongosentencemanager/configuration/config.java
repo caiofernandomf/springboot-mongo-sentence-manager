7b77a477578ccd20ea26ffbed0515fc585c829f5
caiofernandomf <caiofernandomf@gmail.com>
2021-12-13 01:43:57 -0300

    configurações iniciais para o mongo. Adicionando lombok

---
+++ src/main/java/net/springboot/caio/springbootmongosentencemanager/configuration/config.java
@@ -0,0 +1,19 @@
+package net.springboot.caio.springbootmongosentencemanager.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class config extends AbstractMongoClientConfiguration {
+
+    @Override
+    public boolean autoIndexCreation(){
+        return true;
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "Words";
+    }
+}
