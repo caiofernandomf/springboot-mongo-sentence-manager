2a99f88c071647630af88ab5259d59a72b50a3f9
caiofernandomf <caiofernandomf@gmail.com>
2021-12-14 21:23:59 -0300

    Adicionando criação de indíce para busca.

--- src/main/java/net/springboot/caio/springbootmongosentencemanager/model/Word.java
+++ src/main/java/net/springboot/caio/springbootmongosentencemanager/model/Word.java
@@ -3,17 +3,18 @@ package net.springboot.caio.springbootmongosentencemanager.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
-@Document(collection = "Words")
+@Document("Words")
 public class Word {
     @Id
     private String id;
-    @TextIndexed
+    @Indexed(name = "word")
     private String word;
 
     private String sentence;
---
+++ src/main/java/net/springboot/caio/springbootmongosentencemanager/service/WordService.java
@@ -0,0 +1,21 @@
+package net.springboot.caio.springbootmongosentencemanager.service;
+
+import net.springboot.caio.springbootmongosentencemanager.model.Word;
+
+import java.util.List;
+
+public interface WordService {
+    Word createWord(Word word);
+
+    Word updateWord(Word word);
+
+    List<Word> getAllWords();
+
+    Word getByWord(String word);
+
+    List<Word> getAllWordsByLanuage(String language);
+
+    void deleteWord(Word word);
+
+    void deleteWordById(String id);
+}
