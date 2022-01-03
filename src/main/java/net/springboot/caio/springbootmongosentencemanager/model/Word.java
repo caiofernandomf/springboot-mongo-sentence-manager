5a9dc06845c1ec00aa357ce2b9b2842d82d83ae7
caiofernandomf <caiofernandomf@gmail.com>
2022-01-03 00:07:50 -0300

    reescrevendo comparação via método equals

--- src/main/java/net/springboot/caio/springbootmongosentencemanager/model/Word.java
+++ src/main/java/net/springboot/caio/springbootmongosentencemanager/model/Word.java
@@ -2,14 +2,17 @@ package net.springboot.caio.springbootmongosentencemanager.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.IndexDirection;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
+@ToString
 @Document("Words")
 public class Word {
     @Id
@@ -20,4 +23,17 @@ public class Word {
     private String sentence;
     @TextIndexed
     private String idiom;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return word.equals(word1.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word);
+    }
 }
